d1fb0129baaf3426170599d20b725f2661b985a7
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2016-03-17 16:17:50 -0300

    v0.5
    - Tests AI
    - Tests Velocitys to ai

--- app/src/main/java/com/example/emanuelumbelino/jogojamv/MainActivityView.java
+++ app/src/main/java/com/example/emanuelumbelino/jogojamv/MainActivityView.java
@@ -6,7 +6,11 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.Random;
 
 /**
  * Created by Emanuel.Umbelino on 15/03/2016.
@@ -16,24 +20,47 @@ public class MainActivityView extends View implements Runnable
 {
     Context context;
     private Handler handler;
-    private String[] enemiesTypes = new String[2];
-
+    private String[] types = new String[3];
+    private Random rand = new Random();
+    private Ball enemy;
+    private Ball player;
 
     public MainActivityView(Context c)
     {
         super (c);
         context = c;
 
-        enemiesTypes[0] = "Basic";
-        enemiesTypes[1] = "Pusher";
+        types[0] = "Player";
+        types[1] = "Basic";
+        types[2] = "Pusher";
+        enemy = new Ball(rand.nextInt(100),rand.nextInt(100), 10, types[1]);
+        player = new Ball(0,0, 10, types[0]);
 
         handler = new Handler();
     }
 
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+        }
+        return false;
+    }
 
     private void Update()
     {
+        enemy.goToPosition(player.getX(), player.getY(), player.getR());
+    }
 
+    @Override
+    public void run()
+    {
+        handler.postDelayed(this, 30);
+        Update();
+        invalidate();
     }
 
     @Override
@@ -41,47 +68,54 @@ public class MainActivityView extends View implements Runnable
     {
         super.onDraw(canvas);
 
-        Rect ourRect = new Rect();
-        ourRect.set(0, 0, canvas.getWidth(), canvas.getHeight());
-
         Paint paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.FILL);
 
-        canvas.drawCircle(canvas.getWidth()/2, canvas.getHeight()/2, 5, paint);
+        player.setX(canvas.getWidth() / 2);player.setY(canvas.getWidth() / 2);
+        canvas.drawCircle(player.getX(), player.getY(), player.getR(), paint);
 
+        canvas.drawCircle(enemy.getX(), enemy.getY(), enemy.getR(), paint);
     }
 
-    @Override
-    public void run()
-    {
-        handler.postDelayed(this, 30);
-
-        Update();
-        invalidate();
-    }
 }
 
-class Enemy
+class Ball
 {
-    private int x, y, r;
+    private float x, y, r, velX, velY;
     private String type;
 
-    public Enemy(int posX, int posY, int r, String myType)
+    public Ball(int posX, int posY, int ray, String myType)
     {
         x = posX;
         y = posY;
+        r = ray;
         type = myType;
 
     }
+    public void goToPosition(float posY, float posX, float posR)
+    {
+        if((6.67 *0.1*this.r*posR)/(posX-this.x) < 20 && (6.67 *0.1*this.r*posR)/(posX-this.x) > -20)
+        {	this.velX += (6.67 *0.0001 *this.r*posR)/(posX-this.x);}
+        if((6.67 *0.1*this.r*posR)/(posY-this.y) < 20 && (6.67 *0.1*this.r*posR)/(posY-this.y) > -20)
+        {	this.velY += (6.67 *0.0001*this.r*posR)/(posY-this.y);}
+        if(posR < this.r)
+        {
+            this.setY(this.y+this.velY);
+            this.setX(this.x+this.velX);
+        }
+        else
+        {
+            this.setY(this.y-this.velY);
+            this.setX(this.x-this.velX);
+        }
+    }
 
-    public int getX() {
-        return x;
-    }
-    public int getY() {
-        return y;
-    }
-    public int getR() {
+    public float getX() {return x;}
+    public void setX(float x) {this.x = x;}
+    public float getY() {return y;}
+    public void setY(float y) {this.y = y;}
+    public float getR() {
         return r;
     }
 }
